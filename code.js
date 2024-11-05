45d603cab0b83bf7db883a9f9c5ac83f5cdcc0b9
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-11-05 11:00:45 -0700

    First Commit

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function permutationSort(a) {
-    return 0;
+    let counter = 0;
+    let swapped;
+    
+    do {
+        swapped = false;
+        for (var i = 0; i < a.length -1; i++) {
+            if (a[i] > a[i + 1]) {
+                
+                var tmp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = tmp;
+                
+                counter++;
+                swapped = true;
+            }
+        }
+    } while (swapped);  
+
+    return counter;
 }
